ba4484116ca938791fc2b0f38856666986f011f7
thuyha61 <66260179+thuyha61@users.noreply.github.com>
2026-09-07 16:41:50 +0700

    Change map style from open-street-map to carto-positron

--- pages/4_Ban_do_va_Ket_qua_FVI.py
+++ pages/4_Ban_do_va_Ket_qua_FVI.py
@@ -226,7 +226,7 @@ else:
 
         fig_complex.update_layout(
             mapbox=dict(
-                style="open-street-map",
+                style="carto-positron",
                 center=dict(lat=df["CoordY"].mean() if not df.empty else 16.46, lon=df["CoordX"].mean() if not df.empty else 107.60),
                 zoom=12.0
             ),
